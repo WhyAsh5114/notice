091fec04230c87e42dabd782a1e9a349bda89a53
WhyAsh5114 <whyash5114@gmail.com>
2025-11-16 14:09:43 +0000

    feat: add capacitor android

---
+++ android/app/src/main/java/com/whyash5114/notice/MainActivity.java
@@ -0,0 +1,5 @@
+package com.whyash5114.notice;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.whyash5114.notice',
+  appName: 'notice',
+  webDir: 'build'
+};
+
+export default config;
